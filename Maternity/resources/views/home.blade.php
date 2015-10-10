8375706e513a8b61de5d70559d6d8b477ee2427a
DriesVS <van.schevensteen.dries@gmail.com>
2015-10-10 20:34:57 +0200

    Home blade

---
+++ Maternity/resources/views/home.blade.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1,user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>Hello Maternity</title>
+
+		<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	</head>
+	<body>
+		<!--[if lte IE 8]>
+		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
+
+		<header>
+			<div class="search-clothes"> <!-- active -->
+				<a href="">search clothes</a>
+				<div class="search-fields">
+					<div class="distribute">
+						<div><img src="img/icon_pants.svg" alt="pants"></div>
+						<div><img src="img/icon_top.svg" alt="tops"></div>
+						<div><img src="img/icon_dress.svg" alt="dresses"></div>
+						<span class="stretch"></span>
+					</div>
+					<div class="distribute">
+						<div>XS</div>
+						<div>S</div>
+						<div>M</div>
+						<div>L</div>
+						<div>XL</div>
+						<span class="stretch"></span>
+					</div>
+					<div>
+						PRICE
+					</div>
+					<div class="colors">
+						<a href=""><img src="img/colors_all.png" height="26" width="26"></a>
+						<div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+					</div>
+					<div class="search"><a href="">search</a></div>
+				</div>
+			</div>
+			<h1><img src="img/logo.svg" alt="Hello Maternity"></h1>
+			<div class="login">
+				<p><span class="bolder">Welcome Jane Doe</span> <a href="">dashboard</a> | <a href="">logout</a></p>
+				<p><a href=""><span class="bolder">bag</span> (3)</a></p>
+			</div>
+		</header>
+
+		<section class="cover-slider">
+			<div>
+				<a href="" class="arrow left"><img src="img/arrow.svg"></a>
+				<a href="" class="arrow right"><img src="img/arrow.svg"></a>
+			</div>
+		</section>
+
+		<section class="content featured">
+			<h2>Featured Products</h2>
+			<div class="featured-clothes-wrapper">
+				<article>
+					<img src="img/product_1.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_2.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_3.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+				
+				<article>
+					<img src="img/product_1.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_2.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_3.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+				<article>
+					<img src="img/product_1.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_2.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_3.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+				<article>
+					<img src="img/product_1.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_2.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+				<article>
+					<img src="img/product_3.png">
+					<h1>Sweater - ASOS</h1>
+					<div class="price"><span>&euro;</span>90</div>
+					<div class="size">XS</div>
+					<div class="colors">
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</article>
+
+			</div> <!-- end of featured-clothes-wrapper -->
+		</section>
+
+		<footer>
+			<div class="bg-gradient horizontal"></div>
+			<div class="bg-gradient vertical"></div>
+			<p><span class="copy">copyright &copy; 2015 <span class="bolder">Hello Maternity</span></span>
+			design &amp; front-end development <a href="http://driesvanschevensteen.com" target="_blank">Dries Van Schevensteen</a><br>
+			back-end development <a href="http://rubenwouters.be/" target="_blank">Ruben Wouters</a>
+		</footer>
+
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+	</body>
+</html>
