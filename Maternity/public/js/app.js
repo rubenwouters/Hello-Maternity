fee30c6b0681494babdd9d1946a3dd56193af0a1
DriesVS <van.schevensteen.dries@gmail.com>
2015-11-02 20:50:18 +0100

    Search info

--- Maternity/public/js/app.js
+++ Maternity/public/js/app.js
@@ -1,5 +1,19 @@
 (function(){
 	console.log('app.js');
+
+	console.log('.cover-slider');
+	// $('.cover-slider').addClass('second-image');
+
+	$('.cover-slider .left').click(function(){
+		console.log('clicked left');
+		$('.cover-slider').removeClass('second-image');
+
+	});
+
+	$('.cover-slider .right').click(function(){
+		console.log('clicked right');
+		$('.cover-slider').addClass('second-image');
+	});
 })();
 (function(){
 	console.log('search.js');
