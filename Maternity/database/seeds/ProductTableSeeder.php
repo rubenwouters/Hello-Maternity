74f9d6e41ff1540616a189d153ad274121bab508
Ruben Wouters <Ruben@MacBook-Pro-van-Ruben.local>
2015-11-11 17:44:27 +0100

    Seeds changed

--- Maternity/database/seeds/ProductTableSeeder.php
+++ Maternity/database/seeds/ProductTableSeeder.php
@@ -7,15 +7,12 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         $products = array(
-        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'M', 'price' => '50', 'image' => 'product_1.png', 'paid' => 0),
-        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'L', 'price' => '50', 'image' => 'product_2.png', 'paid' => 0),
-        	array('FK_type' => 2, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'S', 'price' => '50', 'image' => 'product_3.png', 'paid' => 0),
-        	array('FK_type' => 2, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'XL', 'price' => '50', 'image' => 'product_1.png', 'paid' => 0),
-        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'XS', 'price' => '50', 'image' => 'product_2.png', 'paid' => 0),
-        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'L', 'price' => '50', 'image' => 'product_3.png', 'paid' => 0),
-        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'M', 'price' => '50', 'image' => 'product_1.png', 'paid' => 0),
-        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'M', 'price' => '50', 'image' => 'product_2.png', 'paid' => 0),
-        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Sweater - ASOS', 'size' => 'M', 'price' => '50', 'image' => 'product_3.png', 'paid' => 0),
+        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Dress - Newlook', 'size' => 'M', 'price' => '15', 'image' => 'product_1.jpg', 'paid' => 0),
+        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Dress - Zara', 'size' => 'M', 'price' => '20', 'image' => 'product_2.jpg', 'paid' => 0),
+        	array('FK_type' => 1, 'FK_user' => 1 , 'brand' => 'Skirt - H&M', 'size' => 'S', 'price' => '95', 'image' => 'product_3.jpg', 'paid' => 0),
+        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Dress - ASOS', 'size' => 'M', 'price' => '45', 'image' => 'product_4.jpg', 'paid' => 0),
+        	array('FK_type' => 3, 'FK_user' => 1 , 'brand' => 'Dress - Mexx', 'size' => 'XS', 'price' => '110', 'image' => 'product_5.jpg', 'paid' => 0),
+        	array('FK_type' => 2, 'FK_user' => 1 , 'brand' => 'Top - ASOS', 'size' => 'L', 'price' => '80', 'image' => 'product_6.jpg', 'paid' => 0),
 		);
 
         DB::table('products')->insert($products);
